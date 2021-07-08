f5705e572bb776bb859a98500ff73bbb7d5bd353
js151 何兴雅 <hxy@tensun.cn>
2021-07-08 19:54:56 +0800

    ggggggggggggg

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,4 +1,4 @@
-#include <QDebug>
+﻿#include <QDebug>
 #include <QSerialPortInfo>
 #include <QSerialPort>
 #include <QMap>
@@ -37,7 +37,7 @@ MainWindow::MainWindow(QWidget* parent) :
     });
     QObject::connect(this->ui->actionAbout, &QAction::triggered, [this]
     {
-        MessageBox::about(this, "关于", "本软件由海滨开发且开放源代码，遵循LGPL协议。");
+        MessageBox::about(this, QStringLiteral("关于"), QStringLiteral("本软件由海滨开发且开放源代码，遵循LGPL协议。"));
     });
     QObject::connect(this->ui->actionAbout_QT, &QAction::triggered, &qApp->aboutQt);
 
@@ -59,11 +59,11 @@ MainWindow::MainWindow(QWidget* parent) :
                 const QByteArray& rcv_data = map_data.value(type);
                 if(type == Serial_RCV)
                 {
-                    rcvStr += "[收] ";
+                    rcvStr += QStringLiteral("[收] ");
                 }
                 else
                 {
-                    rcvStr += "[发] ";
+                    rcvStr += QStringLiteral("[发] ");
                 }
                 rcvStr += QDateTime::fromMSecsSinceEpoch(unix_timestamp).toString("yyyy-MM-dd hh:mm:ss zzz");
                 rcvStr += "\r\n";
@@ -99,7 +99,7 @@ MainWindow::MainWindow(QWidget* parent) :
         QMap <SEND_TYPE, QByteArray> insert_data;
         insert_data.insert(Serial_RCV, rcv_data);
         this->rcv_data_map->insert(unix_timestamp, insert_data);
-        QString rcvStr("[收] ");
+        QString rcvStr(QStringLiteral("[收] "));
         rcvStr += QDateTime::fromMSecsSinceEpoch(unix_timestamp).toString("yyyy-MM-dd hh:mm:ss zzz");
         rcvStr += "\r\n";
 
@@ -140,9 +140,9 @@ MainWindow::MainWindow(QWidget* parent) :
             {
                 serial_port->close();
             }
-            if(this->ui->pB_openSerail->text() == "关闭")
+            if(this->ui->pB_openSerail->text() == QStringLiteral("关闭"))
             {
-                this->ui->pB_openSerail->setText("打开");
+                this->ui->pB_openSerail->setText(QStringLiteral("打开"));
             }
             if(!this->ui->cB_serailport->isEnabled())
             {
@@ -171,7 +171,7 @@ MainWindow::MainWindow(QWidget* parent) :
             {
 
                 qDebug() << "设置波特率失败，失败原因：" << serial_port->errorString();
-                MessageBox::warning(this, "设置波特率失败", QString("原因:") + serial_port->errorString());
+                MessageBox::warning(this, QStringLiteral("设置波特率失败"), QStringLiteral("原因:") + serial_port->errorString());
                 return;
             }
             // 获取数据位
@@ -181,7 +181,7 @@ MainWindow::MainWindow(QWidget* parent) :
             {
 
                 qDebug() << "设置数据位失败，失败原因：" << serial_port->errorString();
-                MessageBox::warning(this, "设置数据位失败", QString("原因:") + serial_port->errorString());
+                MessageBox::warning(this, QStringLiteral("设置数据位失败"), QStringLiteral("原因:") + serial_port->errorString());
                 return;
             }
             // 获取停止位
@@ -191,7 +191,7 @@ MainWindow::MainWindow(QWidget* parent) :
             {
 
                 qDebug() << "设置停止位失败，失败原因：" << serial_port->errorString();
-                MessageBox::warning(this, "错误", QString("设置停止位失败，失败原因：") + serial_port->errorString());
+                MessageBox::warning(this, QStringLiteral("错误"), QStringLiteral("设置停止位失败，失败原因：") + serial_port->errorString());
                 return;
             }
             // 获取校验位
@@ -201,7 +201,7 @@ MainWindow::MainWindow(QWidget* parent) :
             {
 
                 qDebug() << "设置校验位失败，失败原因：" << serial_port->errorString();
-                MessageBox::warning(this, "设置校验位失败", QString("原因:") + serial_port->errorString());
+                MessageBox::warning(this, QStringLiteral("设置校验位失败"), QStringLiteral("原因:") + serial_port->errorString());
                 return;
             }
             // 设置流控
@@ -210,14 +210,14 @@ MainWindow::MainWindow(QWidget* parent) :
             {
 
                 qDebug() << "设置流控失败，失败原因：" << serial_port->errorString();
-                MessageBox::warning(this, "设置流控失败", QString("原因:") + serial_port->errorString());
+                MessageBox::warning(this, QStringLiteral("设置流控失败"), QStringLiteral("原因:") + serial_port->errorString());
                 return;
             }
             // 设置读写
             ret &= serial_port->open(QSerialPort::ReadWrite);
             if(ret)
             {
-                this->ui->pB_openSerail->setText("关闭");
+                this->ui->pB_openSerail->setText(QStringLiteral("关闭"));
                 if(this->ui->cB_serailport->isEnabled())
                 {
                     this->ui->cB_serailport->setEnabled(false);
@@ -229,7 +229,7 @@ MainWindow::MainWindow(QWidget* parent) :
             else
             {
                 qDebug() << "自定义MessageBox提示打开错误" << serial_port->errorString();
-                MessageBox::warning(this, "打开串口失败", QString("原因:") + serial_port->errorString());
+                MessageBox::warning(this, QStringLiteral("打开串口失败"), QStringLiteral("原因:") + serial_port->errorString());
             }
         }
     });
@@ -242,7 +242,7 @@ MainWindow::MainWindow(QWidget* parent) :
     QObject::connect(ui->pB_autoSend, &QPushButton::clicked, [this]
     {
 
-        if(this->ui->pB_autoSend->text() == "自动发送")
+        if(this->ui->pB_autoSend->text() == QStringLiteral("自动发送"))
         {
             bool is_ok = false;
             int send_inter_ms = this->ui->lE_send_ms->text()
@@ -254,14 +254,14 @@ MainWindow::MainWindow(QWidget* parent) :
 
 //                this->send_timer->setInterval(send_inter_ms);
                 this->send_timer->start(send_inter_ms);
-                this->ui->pB_autoSend->setText("停止发送");
+                this->ui->pB_autoSend->setText(QStringLiteral("停止发送"));
                 this->ui->pB_senddata->setDisabled(true);
                 this->ui->pB_sendfile->setDisabled(true);
             }
             else
             {
                 qDebug() << "报错";
-                MessageBox::warning(this, "错误", "请查看串口是否打开以及时间间隔和内容是否设置！");
+                MessageBox::warning(this, QStringLiteral("错误"), QStringLiteral("请查看串口是否打开以及时间间隔和内容是否设置！"));
             }
 
         }
@@ -269,7 +269,7 @@ MainWindow::MainWindow(QWidget* parent) :
         {
             this->ui->pB_senddata->setDisabled(false);
             this->ui->pB_sendfile->setDisabled(false);
-            this->ui->pB_autoSend->setText("自动发送");
+            this->ui->pB_autoSend->setText(QStringLiteral("自动发送"));
             this->send_timer->stop();
         }
     });
@@ -285,7 +285,7 @@ MainWindow::MainWindow(QWidget* parent) :
     QObject::connect(this->ui->pB_sendfile, &QPushButton::clicked, [this]
     {
         if(!this->is_open_serailport) return;
-        QString fileName = QFileDialog::getOpenFileName(this, "打开文件");
+        QString fileName = QFileDialog::getOpenFileName(this, QStringLiteral("打开文件"));
         if(!fileName.isEmpty())
         {
             QFile file(fileName);
@@ -301,7 +301,7 @@ MainWindow::MainWindow(QWidget* parent) :
                 const quint64& unix_timestamp = QDateTime::currentMSecsSinceEpoch();   //获取当前时间
                 insert_data.insert(Serial_SEND, send_buffer);
 
-                QString sendStr("[发] ");
+                QString sendStr(QStringLiteral("[发] "));
                 sendStr += QDateTime::fromMSecsSinceEpoch(unix_timestamp).toString("yyyy-MM-dd hh:mm:ss zzz");
                 sendStr += "\r\n";
 
@@ -340,7 +340,7 @@ MainWindow::MainWindow(QWidget* parent) :
 
                 QScrollBar* scrollBar = this->ui->tE_rcvcon->verticalScrollBar();
                 scrollBar->setValue(scrollBar->maximum());
-                MessageBox::info(this, "成功", "发送文件成功");
+                MessageBox::info(this, QStringLiteral("成功"), QStringLiteral("发送文件成功"));
             }
 
             file.close();
@@ -408,7 +408,7 @@ void MainWindow::initBautrateList()
             {
                 qDebug() << "切换失败";
                 this->ui->cB_bautrate->setCurrentIndex(this->last_combox_index);
-                MessageBox::warning(this, "错误", "切换失败，可能当前硬件不支持这种波特率！");
+                MessageBox::warning(this, QStringLiteral("错误"), QStringLiteral("切换失败，可能当前硬件不支持这种波特率！"));
             }
         }
     });
@@ -440,7 +440,7 @@ void MainWindow::initDataBitList()
             {
                 qDebug() << "切换失败";
                 this->ui->cB_databits->setCurrentIndex(this->last_combox_index);
-                MessageBox::warning(this, "错误", "切换失败，可能当前硬件不支持这种数据位！");
+                MessageBox::warning(this, QStringLiteral("错误"), QStringLiteral("切换失败，可能当前硬件不支持这种数据位！"));
 //                return false;
             }
         }
@@ -468,7 +468,7 @@ void MainWindow::initStopBitList()
             if(!ret)
             {
                 qDebug() << "切换失败";
-                MessageBox::warning(this, "错误", "切换失败，可能当前硬件不支持这种停止位！");
+                MessageBox::warning(this, QStringLiteral("错误"), QStringLiteral("切换失败，可能当前硬件不支持这种停止位！"));
                 this->ui->cB_stopbit->setCurrentIndex(this->last_combox_index);
 //                this->ui->cB_stopbit->
 //                return false;
@@ -480,16 +480,16 @@ void MainWindow::initStopBitList()
 void MainWindow::initCheckBitList()
 {
     QMap<QString, QSerialPort::Parity> checkbit_list;
-    checkbit_list.insert("无校验", QSerialPort::NoParity);
-    checkbit_list.insert("奇校验", QSerialPort::OddParity);
-    checkbit_list.insert("偶校验", QSerialPort::EvenParity);
-    checkbit_list.insert("1 校验", QSerialPort::MarkParity);
-    checkbit_list.insert("0 校验", QSerialPort::SpaceParity);
+    checkbit_list.insert(QStringLiteral("无校验"), QSerialPort::NoParity);
+    checkbit_list.insert(QStringLiteral("奇校验"), QSerialPort::OddParity);
+    checkbit_list.insert(QStringLiteral("偶校验"), QSerialPort::EvenParity);
+    checkbit_list.insert(QStringLiteral("1 校验"), QSerialPort::MarkParity);
+    checkbit_list.insert(QStringLiteral("0 校验"), QSerialPort::SpaceParity);
     foreach(const QString& key, checkbit_list.keys())
     {
         ui->cB_checkbit->addItem(key, checkbit_list[key]);
     }
-    ui->cB_checkbit->setCurrentText("无校验");
+    ui->cB_checkbit->setCurrentText(QStringLiteral("无校验"));
     ui->cB_checkbit->installEventFilter(this);
     QObject::connect(ui->cB_checkbit, &QComboBox::currentTextChanged, [this](QString)
     {
@@ -500,7 +500,7 @@ void MainWindow::initCheckBitList()
             if(!ret)
             {
                 qDebug() << "切换失败";
-                MessageBox::warning(this, "错误", "切换失败，可能当前硬件不支持这种校验位！");
+                MessageBox::warning(this, QStringLiteral("错误"), QStringLiteral("切换失败，可能当前硬件不支持这种校验位！"));
                 this->ui->cB_checkbit->setCurrentIndex(this->last_combox_index);
 //                return false;
             }
@@ -638,7 +638,7 @@ void MainWindow::slotOnSendSerialContent()
                 const quint64& unix_timestamp = QDateTime::currentMSecsSinceEpoch();   //获取当前时间
                 insert_data.insert(Serial_SEND, send_arr);
 
-                QString sendStr("[发] ");
+                QString sendStr(QStringLiteral("[发] "));
                 sendStr += QDateTime::fromMSecsSinceEpoch(unix_timestamp).toString("yyyy-MM-dd hh:mm:ss zzz");
                 sendStr += "\r\n";
 
@@ -689,7 +689,7 @@ void MainWindow::slotOnSendSerialContent()
 void MainWindow::slotOnSaveData()
 {
 
-    QString fileName = QFileDialog::getSaveFileName(this, "保存文件");
+    QString fileName = QFileDialog::getSaveFileName(this, QStringLiteral("保存文件"));
     if(!fileName.isEmpty())
     {
         QFile file(fileName);
@@ -730,7 +730,7 @@ void MainWindow::slotOnSaveData()
         }
         w_stream.writeRawData(w_data.data(), w_data.length());
         file.close();
-        MessageBox::info(this, "成功", "保存文件成功");
+        MessageBox::info(this, QStringLiteral("成功"), QStringLiteral("保存文件成功"));
     }
 }
 
